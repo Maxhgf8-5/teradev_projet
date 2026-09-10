920997538bad52b261c847d5de7dbc523affd98d
Gnansa Maxime <gnansamaxime1@gmail.com>
2026-09-10 00:06:33 +0000

    Config github page

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react(),    tailwindcss(),],
+  base: '/TERADEV',
 
   server: {
     host: '0.0.0.0',
+    port: 5173,
     allowedHosts: ['teradev.local'],
   },
 })
